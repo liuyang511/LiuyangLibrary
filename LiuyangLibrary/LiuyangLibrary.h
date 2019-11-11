e991591eb40c85b4a081560568aa03bc760be004
liuyang <liuyang@housecenter.cn>
2019-11-11 11:22:26 +0800

    no message

---
+++ LiuyangLibrary/LiuyangLibrary.h
@@ -0,0 +1,16 @@
+//
+//  LiuyangLibrary.h
+//  LiuyangLibrary
+//
+//  Created by 非请勿入 on 2019/11/11.
+//  Copyright © 2019 liuyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LiuyangLibrary : NSObject
+
+/// 测试输出一段文字
+- (void)textOutput;
+
+@end
